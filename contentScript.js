97a281e749b9cf5d14846d483d2820cc0b76657f
김효빈 <52522662+hbin12212@users.noreply.github.com>
2022-06-02 16:50:31 +0900

    [FEAT] : 크롬 익스텐션 popup 띄우기

---
+++ contentScript.js
@@ -0,0 +1,7 @@
+// const intervalForBackgroundChange = setInterval(() => {
+//     const isBodyLoaded = !!document.body;
+//     if (isBodyLoaded) {
+//         clearInterval(intervalForBackgroundChange);
+//         document.body.style.backgroundColor = "skyblue";
+//     }
+// }, 10);
