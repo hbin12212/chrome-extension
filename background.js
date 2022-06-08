ab7cb0aed775bdf9dc2c6cbe6cf265aceedd6dee
김효빈 <52522662+hbin12212@users.noreply.github.com>
2022-06-08 17:58:03 +0900

    [FEAT] : 클릭이벤트 추가

--- background.js
+++ background.js
@@ -1,7 +1,27 @@
+//우클릭 메뉴 아이템 설정
 let contextMenuItem = {
     id: "pming",
     title: "pming",
     contexts: ["page"],
 };
 
+//우클릭 메뉴 생성
 chrome.contextMenus.create(contextMenuItem);
+
+//크롬 익스텐스 아이콘 클릭 -> 현재 링크 복사
+chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+        let url = tabs[0].url;
+        console.log(url);
+    });
+});
+
+//우클릭 메뉴 클릭 -> 현재 링크 복사
+chrome.contextMenus.onClicked.addListener((tab) => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+        let url = tabs[0].url;
+        console.log(url);
+    });
+});
+
+//New Tab 설정 (manifest.json)
